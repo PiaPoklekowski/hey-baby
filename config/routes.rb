ca570e809ea657a1c015b943229846cef7895a51
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-05-31 15:57:46 +0200

    creates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'profiles/edit', to: 'profiles#edit', as: :edit_profile
+  patch 'profiles', to: 'profiles#update'
+  get 'profiles', to: 'profiles#show', as: :profile
+  resources :pregnancies, only: [:new, :create]
+  resources :important_contacts, only: [:new, :create]
+  resources :categories, only: [:index, :show] do
+    resources :tasks, only: [:show, :update]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
