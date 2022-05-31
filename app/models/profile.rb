839d04850470e49fe87363d549f47b31cc4a3195
Alicia de Biolley <aliciadb@gmail.com>
2022-05-31 16:15:31 +0200

    adds add_profile method on user controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :address, presence: true
-  validates :birthdate, presence: true
-  validates :role, presence: true
+
 end
