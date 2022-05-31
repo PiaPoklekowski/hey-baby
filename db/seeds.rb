ac2c6a4bfb7638ce9bd8e1d797e683fdc53e25af
Alicia de Biolley <aliciadb@gmail.com>
2022-05-31 18:02:40 +0200

    generates first seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,120 @@ Task.delete_all
 User.delete_all
 Pregnancy.delete_all
 
-  User.create(
+CATEGORIES = ["Birth certificate",
+  "Breastfeeding",
+  "Child Benefit",
+  "Embassy",
+  "Gynecologist",
+  "Health",
+  "Health Insurance",
+  "Hospital",
+  "Kita - Kindergarten",
+  "Maternity Allowance",
+  "Maternity Leave",
+  "Midwive",
+  "Organisational",
+  "Parental Allowance",
+  "Parental Leave",
+  "Paternity",
+  "Pediatrician",
+  "Photoshoot",
+  "Prenatal Classes"]
+
+user_one = User.create(
     email: "heybabyberlin@gmail.com",
     password: "123456"
   )
+
+  pregancy_one = Pregnancy.create(
+    due_date: Date.new(2022,12,15),
+    baby_nickname: "Tupac"
+    pregnancy_week: 11,
+    gender: "boy",
+    mother: user_one
+  )
+
+
+CATEGORIES.each do |category|
+  Category.create(
+    name: category,
+    description: category
+  )
+end
+
+hospital_category = Category.find_by(name: "Hospital")
+child_benefit_category = Category.find_by(name: "Child Benefit")
+birth_certificate_category = Category.find_by(name: "Birth certificate")
+
+Task.create(
+  name: "Find an hospital",
+  description: "If you wish to give birth at a birthing centre or elsewhere with a midwife in attendance, early application is needed.
+                You can register later at a hospital. This registration is not binding.",
+  start_time: 20,
+  deadline: 22,
+  form_url_de:
+  form_url_en:
+  category: hospital_category
+  pregnancy: pregancy_one
+)
+
+Task.create(
+  name: "Book an appointment"
+  description: "Book an appointment for the registration at the hospital of your choice",
+  start_time: 22,
+  deadline: 25,
+  form_url_de:
+  form_url_en:
+  category: hospital_category
+  pregnancy: pregancy_one
+)
+
+Task.create(
+  name: "Visit and register in you Hospital"
+  description: "Onsite interview with a Midwife. Explain your birth type wishes and questions (waterbirth, natural birth..., room wishes)",
+  start_time: 22,
+  deadline: 25,
+  form_url_de:
+  form_url_en:
+  category: hospital_category
+  pregnancy: pregancy_one
+)
+
+Task.create(
+  name: "Applying for child benefit (Kindergeld)",
+  description: "As soon as the birth certificate is available - Create an account and fill in the form, print, sign and scan and send it to the given address",
+  start_time: 44,
+  deadline:
+  form_url_de: "https://web.arbeitsagentur.de/opal/kgo-antraggeburt-ui/auswahl",
+  form_url_en:
+  category: child_benefit_category
+  pregnancy: pregancy_one
+)
+
+Task.create(
+  name: "Find mother & father birth certificates",
+  description: "Ask in your homecountry",
+  start_time: 30,
+  deadline: 36,
+  form_url_de:
+  form_url_en:
+  category: birth_certificate_category
+  pregnancy: pregancy_one
+)
+
+Task.create(
+  name: "Registering the child to the registry office (Standesamt)",
+  description: "Within a week after the birth, register your baby to the registery office in which your baby is born or directly at the hospital. They will issue the birth certificate.
+                Required documents:
+                  -Birth announcement will be send by the hospital
+                  -Declaration of both parents about the first name and surname of the child (signed)
+                  -Valid identity card or passport of parents
+                  - mother's and father's birth certificate
+                  - if applicable, marriage certificate",
+  start_time: 40,
+  deadline: 41,
+  form_url_de: "https://drive.google.com/file/d/1VkinWa9dQFZGq0KeL-FRjqTU7_-uRa_X/view?usp=sharing",
+  form_url_en: "https://drive.google.com/file/d/1l03b7gN0ovwR5dikkgpGN8BmsYJ92AO3/view?usp=sharing",
+  category: birth_certificate_category
+  pregnancy: pregancy_one
+)
