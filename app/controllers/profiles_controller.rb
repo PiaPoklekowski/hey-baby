c43e5b438cd5ff7ca61c936e57a79793d6840186
Antonia Lappin <antonia.lappin@gmx.de>
2022-05-31 16:39:24 +0200

    edit and update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,11 @@
 class ProfilesController < ApplicationController
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.update(params[:profile])
+    redirect_to profile_path(@profile)
+  end
 end
