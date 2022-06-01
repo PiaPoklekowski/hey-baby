07812bc9d5f66fc0c5d2d04a97683a3798295047
Antonia Lappin <antonia.lappin@gmx.de>
2022-06-01 10:44:16 +0200

    edit and update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,19 @@
 class ProfilesController < ApplicationController
   def edit
     @profile = Profile.find(params[:id])
+    authorize @profile
   end
 
   def update
     @profile = Profile.find(params[:id])
-    @profile.update(params[:profile])
+    authorize @profile
+    @profile.update(profile_params)
     redirect_to profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :address, :birthdate, :role)
   end
 end
