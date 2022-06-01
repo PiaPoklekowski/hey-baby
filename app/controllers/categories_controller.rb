e268e1f4f4945d9c721a3d7cd9c662ac133f599f
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-06-01 17:12:37 +0200

    adds logic for completed category tasks

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,5 @@ class CategoriesController < ApplicationController
   def index
     @categories = policy_scope(Category)
     @categories = Category.all
-    # @tasks = Task.find(params[:category_id])
   end
 end
