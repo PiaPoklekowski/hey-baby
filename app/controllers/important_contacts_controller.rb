a659a325e045d71c2394c31c0783b98e4bd3fcf1
PiaPoklekowski <poklekowskipia@gmail.com>
2022-06-01 12:11:01 +0200

    create pregnancy without partner

--- app/controllers/important_contacts_controller.rb
+++ app/controllers/important_contacts_controller.rb
@@ -1,2 +1,10 @@
 class ImportantContactsController < ApplicationController
+  def new
+    @pregnancy = current_user.current_pregnancy
+    @important_contact = ImportantContact.new
+    authorize @important_contact
+  end
+
+  def create
+  end
 end
--- app/controllers/pregnancies_controller.rb
+++ app/controllers/pregnancies_controller.rb
@@ -2,16 +2,23 @@ class PregnanciesController < ApplicationController
 
   def new
     @pregnancy = Pregnancy.new
+    authorize @pregnancy
   end
 
   def create
     @pregnancy = Pregnancy.new(pregnancy_params)
     @pregnancy.mother = current_user
+    authorize @pregnancy
+    if @pregnancy.save
+      redirect_to new_important_contact_path(@pregnancy)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def pregnancy_params
-    params.require(:pregnancy).permit(:pregnancy_week, :due_date, :baby_nickname, :gender, :partner)
+    params.require(:pregnancy).permit(:pregnancy_week, :due_date, :baby_nickname, :gender)
   end
 end
