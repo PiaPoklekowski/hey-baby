2a49e1c08e9b7ce3f6e0d4b6d48c5938d3c8a53f
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-06-02 16:52:00 +0200

    adds dynamic content to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @tasks = policy_scope(Task)
+    @tasks = Task.where(completed: false)
   end
 
-  
+
 end
