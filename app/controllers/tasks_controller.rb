e5637e6145fa704110d2d3200081aeac45951fc4
Alicia de Biolley <aliciadb@gmail.com>
2022-06-02 15:54:11 +0200

    adds autocomplete address to profile

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,16 @@
 class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
-    authorize @task
-    if @task.name == "Find a hospital"
-      @user_marker = [current_user.profile].geocoded.map { |address| {lat: address.latitude, lng: address.longitude } }
+    profile = current_user.profile
+    if @task.name == "Find a hospital" && profile.geocoded?
+      @user_marker = [{ lat: profile.latitude, lng: profile.longitude }]
       @markers = Hospital.all.geocoded.map do |hospital|
         {
           lat: hospital.latitude,
-          lng: hospital.longitude
+          lng: hospital.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { hospital: hospital })
         }
+      @all_markers = @markers.push(@user_marker)
       end
     end
   end
