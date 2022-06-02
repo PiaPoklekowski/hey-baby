61f9e4e0b8a78744fa2adf513b3e43424f8c2e42
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-06-02 18:42:11 +0200

    adds links to dashboard

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
     @tasks = policy_scope(Task)
   end
+
+  def show
+    # @task = Task.where(pregnancy: current_user.current_pregnancy).and(Task.find(params[:category_id]))
+    # authorize @task
+  end
 end
