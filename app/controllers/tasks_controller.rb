707994bd0ef4963ca3178dd644a2ff2afd4378f3
PiaPoklekowski <poklekowskipia@gmail.com>
2022-06-03 15:34:46 +0200

    pins

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,14 +9,14 @@ class TasksController < ApplicationController
           lat: profile.latitude,
           lng: profile.longitude,
           info_window: render_to_string(partial: "info_window_profile", locals: { profile: profile }),
-          image_url: helpers.asset_url("/assets/Pin_Home.png")
+          image_url: helpers.asset_url("Pin_Home.png")
         }
       @markers = Hospital.all.geocoded.map do |hospital|
         {
           lat: hospital.latitude,
           lng: hospital.longitude,
           info_window: render_to_string(partial: "info_window", locals: { hospital: hospital }),
-          image_url: helpers.asset_url("/assets/Pin_Hospital.png")
+          image_url: helpers.asset_url("Pin_Hospital.png")
         }
       end
       @all_markers = @markers.push(@user_marker)
