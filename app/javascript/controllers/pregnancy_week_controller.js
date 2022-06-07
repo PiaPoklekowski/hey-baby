793aa75dc8436c33f7d7c7d275e31a4963eb3d12
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-06-07 12:04:07 +0200

    selects week of pregnancy

--- app/javascript/controllers/pregnancy_week_controller.js
+++ app/javascript/controllers/pregnancy_week_controller.js
@@ -5,30 +5,44 @@ export default class extends Controller {
   static targets = ["pregnancy", "trimester"]
 
   connect() {
-    console.log(this.trimesterTargets)
-    let pregnancyWeek = this.pregnancyTarget.innerText;
+    let pregnancyWeek = parseInt(this.pregnancyTarget.innerText);
+    let trimester = this.setTrimester(pregnancyWeek);
+    trimester.classList.toggle("current-weeks");
 
-    let trimester;
+    this.setWeek(pregnancyWeek, trimester)
+  }
+
+  setTrimester(pregnancyWeek) {
+    let trimester = 0;
     if (pregnancyWeek < 4) {
       trimester = this.trimesterTargets[0]
     } else if (pregnancyWeek > 13 && pregnancyWeek < 28) {
       trimester = this.trimesterTargets[1]
     } else {
       trimester = this.trimesterTargets[2]
-    };
-
-    trimester.classList.toggle("current-weeks");
-
-    let week1 = trimester.children[0];
-    let week2 = trimester.children[1];
-    let week3 = trimester.children[2];
-
-    if (week1.innerText.includes(String(pregnancyWeek))) {
-      week1.classList.add("active-week")
-    } else if (week2.innerText.includes(String(pregnancyWeek))) {
-      week2.classList.add("active-week")
-    } else if (week3.innerText.includes(String(pregnancyWeek))) {
-      week3.classList.add("active-week")
     }
+    return trimester;
+  }
+
+  setWeek(pregnancyWeek, trimester) {
+    const weeks1 = trimester.children[0].innerText.split("-");
+    const weeks2 = trimester.children[1].innerText.split("-");
+    const weeks3 = trimester.children[2].innerText.split("-");
+
+    if (this.range(weeks1).includes(pregnancyWeek)) {
+      trimester.children[0].classList.add("active-week")
+    } else if (this.range(weeks2).includes(pregnancyWeek)) {
+      trimester.children[1].classList.add("active-week")
+    } else if (this.range(weeks3).includes(pregnancyWeek)) {
+      trimester.children[2].classList.add("active-week")
+    }
+  }
+
+  range(weeks) {
+    const rangeArray = []
+    for (let i = parseInt(weeks[0]); i <= parseInt(weeks[1]); i++) {
+      rangeArray.push(i);
+    }
+    return rangeArray;
   }
 }
